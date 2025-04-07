3d927b7c10b93499297da1939daaebd241e01165
Dizan <dizanp@gmail.com>
2025-04-07 16:40:07 +0700

    upadete_auth

--- Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
+++ Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e335535c7032f24f54baaf7e26925ecc9acca0b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6cb12a455ed903bda4ef1306459d831ee9303b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
